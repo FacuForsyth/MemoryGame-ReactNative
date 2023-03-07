db448d2db093c42f71498a32ede18dbbf4cf0c87
FacuForsyth <framirezforsyth@gmail.com>
2023-03-07 16:39:44 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,92 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Card from './Card';
+import ConfettiCannon from 'react-native-confetti-cannon'
+
+const cards = [
+"⚽️",
+"🏀",
+"🏈",
+"⚾️",
+"🥎",
+"🏐",
+/* "🎱",
+"🥊",
+"🏓", */
+
+/* "🏑",
+"🎾",
+"🥌", */   
+];
+
+//funcion para desordenar/revolver las tarjetas - random
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    //cambia los elementos al index i y randomIndex
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  return array;
+}
 
 export default function App() {
+
+  //la card tiene que estar duplicada para poder encontrar la igual
+  const [board, setBoard] = React.useState(() => shuffle([...cards, ...cards]));
+  const [selectedCards, setSelectedCards] = React.useState([]);
+  const [matchedCards, setMatchedCards] = React.useState([]);
+  const [score, setScore] = React.useState(0);
+
+  React.useEffect(() => {
+    if (selectedCards.length < 2) return;
+    //comparar las tarjetas si son iguales -> usado los index
+    if (board[selectedCards[0]] === board[selectedCards[1]]) {
+      setMatchedCards([...matchedCards, ...selectedCards]);
+      setSelectedCards([]);
+    } else {
+      //crear un time out para que gire la carta cuando no son iguales
+      const timeoutId = setTimeout(() => setSelectedCards([]), 1000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [selectedCards]);
+
+  const handleTapCard = (index) => {
+    if (selectedCards.length >= 2 || selectedCards.includes(index)) return;
+    setSelectedCards([...selectedCards, index]);
+    setScore(score + 1);
+  };
+
+  const didPlayerWin = () => matchedCards.length === board.length;
+
+  const resetGame = () => {
+    setMatchedCards([]);
+    setScore(0);
+    setSelectedCards([]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {didPlayerWin() ? <ConfettiCannon count={50} origin={{x: -10, y: 0}} /> : null}
+      <Text style={styles.title}>
+        {didPlayerWin() ? "Congratulations 🏆" : "Memory Game"}
+      </Text>
+      <Text style={styles.title}>Score: {score}</Text>
+      <View style={styles.board}>
+        {board.map((card, index) => {
+          const isTurnedOver = 
+            selectedCards.includes(index) || matchedCards.includes(index);
+          return (
+            <Card 
+              key={index}
+              isTurnedOver={isTurnedOver}
+              onPress={() => handleTapCard(index)}
+            >{card}</Card>
+          )
+        })}
+      </View>
+      {didPlayerWin() && <Button onPress={resetGame} title="reset" />}
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +94,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f172a',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 32,
+    color: "white",
+    fontWeight: "900",
+  },
+  board: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  }
 });
